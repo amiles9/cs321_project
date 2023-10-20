a042117968bc7bacd2b1fe9fde31243689a055ee
ekrmkaya <ekaya4@gmu.edu>
2023-10-19 22:53:14 -0400

    workflow class

--- WorkflowTable.java
+++ WorkflowTable.java
@@ -1,15 +1,37 @@
+import java.util.ArrayDeque;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class WorkflowTable{
 
     private Queue<Integer> reviewer_queue;
     private Queue<Integer> approver_queue;
 
     public WorkflowTable(){
-        reviewer_queue = new Queue<Integer>();
-        approver_queue = new Queue<Integer>();
+        reviewer_queue = new ArrayDeque<Integer>();
+        approver_queue = new ArrayDeque<Integer>();
     }
 
-    public add_form(int id){
-        
+    public void add_review_form(int id){
+        this.reviewer_queue.add(id);
+    }
+
+    public void add_approve_form(int id){
+        this.approver_queue.add(id);
+    }
+
+    public int next_review_form(){
+        if(reviewer_queue.peek() == null){
+            return -1;
+        }
+        else return reviewer_queue.poll();
+    }
+
+    public int next_approve_form(){
+        if(approver_queue.peek() == null){
+            return -1;
+        }
+        else return approver_queue.poll();
     }
 
 }
