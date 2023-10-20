aeb10a5e926a4989bd884b2a4ae5ba4105c2f435
ekrmkaya <ekaya4@gmu.edu>
2023-10-19 22:41:54 -0400

    workflow class

---
+++ WorkflowTable.java
@@ -0,0 +1,15 @@
+public class WorkflowTable{
+
+    private Queue<Integer> reviewer_queue;
+    private Queue<Integer> approver_queue;
+
+    public WorkflowTable(){
+        reviewer_queue = new Queue<Integer>();
+        approver_queue = new Queue<Integer>();
+    }
+
+    public add_form(int id){
+        
+    }
+
+}
